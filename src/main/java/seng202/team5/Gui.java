29e55e00132c6bd36f45e76549c2437fa2ce9d50
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-22 00:04:50 +1200

    Added the content of the Gui class as specified in the UML Class Diagram

---
+++ src/main/java/seng202/team5/Gui.java
@@ -0,0 +1,21 @@
+package seng202.team5;
+//TODO Are we going to do the GUI using MVC, this file may become obsolete.
+
+public class Gui {
+    private User currentUser;
+    private DataFetcher database;
+    private FileHandler localFiles;
+    private Map map;
+
+    public void loginUser(User user) {
+
+    }
+
+    public void connectDb() {
+
+    }
+
+    public void loadLocal() {
+
+    }
+}
