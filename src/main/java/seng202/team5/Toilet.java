9250273f1266ee25a3f5edfa88655df4e2d4a8c2
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-21 14:15:54 +1200

    Added the content of the Toilet class as specified in the UML Class Diagram

--- src/main/java/seng202/team5/Toilet.java
+++ src/main/java/seng202/team5/Toilet.java
@@ -1,4 +1,22 @@
 package seng202.team5;
 
-public class Toilet {
+
+public class Toilet extends Location {
+    private boolean forDisabled;
+    private boolean uniSex;
+
+    public Toilet(double lattitude, double longitude, String name, boolean forDisabled, boolean uniSex) {
+        super(lattitude, longitude, name, "Toilet");
+        this.forDisabled = forDisabled;
+        this.uniSex = uniSex;
+    }
+
+    public boolean getForDisabled() {
+        return this.forDisabled;
+    }
+
+    public boolean getUniSex() {
+        return this.uniSex;
+    }
 }
+
