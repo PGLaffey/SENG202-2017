1e8196d13f43b69f13bdd2b58dd039913270db30
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-21 14:21:51 +1200

    Added the content of the Location class as specified in the UML Class Diagram

--- src/main/java/seng202/team5/Location.java
+++ src/main/java/seng202/team5/Location.java
@@ -1,4 +1,43 @@
 package seng202.team5;
 
+
 public class Location {
+    private double lattitude;
+    private double longitude;
+    //TODO added type to location
+    private String type;
+    private int id;
+    private String name;
+    private boolean local;
+
+    //Added name and type to the location builder, is this correct?
+    public Location(double latitude, double longitude, String name, String type) {
+        this.lattitude = latitude;
+        this.longitude = longitude;
+        this.name = name;
+        this.type = type;
+    }
+
+    //Should this be an array rather than a List?
+    public List<double> getCoords() {
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    //TODO Add this to the UML diagram
+    public int getId() {
+        return this.id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //TODO Should this be set?
+    public void storeLocal(boolean local) {
+        this.local = local;
+    }
 }
