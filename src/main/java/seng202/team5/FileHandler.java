d4c4b7f44f9e3a86e7461c636686fb9303ad7766
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-22 00:01:23 +1200

    Added the content of the FileHandler class as specified in the UML Class Diagram

---
+++ src/main/java/seng202/team5/FileHandler.java
@@ -0,0 +1,23 @@
+package seng202.team5;
+
+import java.io.FileReader;
+
+public class FileHandler {
+    private FileReader reader;
+
+    public void loadRoute(Route route) {
+
+    }
+
+    public void loadLocation(Route route) {
+
+    }
+
+    public String readFile(String direct) {
+        return direct;
+    }
+
+    public void writeFile(String direct, String contents) {
+
+    }
+}
