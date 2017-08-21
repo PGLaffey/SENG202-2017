f3d4489ad4cc131d043d107c7acea4ca47baf484
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-21 14:12:42 +1200

    Added the content of the Route class as specified in the UML Class Diagram

--- src/main/java/seng202/team5/Route.java
+++ src/main/java/seng202/team5/Route.java
@@ -1,4 +1,49 @@
 package seng202.team5;
 
 public class Route {
+    private String name;
+    private int id;
+    private double distance;
+    private boolean local;
+    private Location start;
+    private Location end;
+
+    public Route(Location start, Location end, String name) {
+        this.start = start;
+        this.end = end;
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    //TODO Add getId(void) to the UML diagram
+    public int getId() {
+        return this.id;
+    }
+
+    public double getDistance() {
+        return this.distance;
+    }
+
+    //TODO Add getLocal to UML diagram
+    public boolean getLocal() {
+        return this.local;
+    }
+
+    public Location getStart() {
+        return this.start;
+    }
+
+    //TODO Add getEnd(void) to the UML diagram, getStart added twice
+    public Location getEnd() {
+        return this.end;
+    }
+
+    //TODO Should this be setLocation?
+    public void storeLocation(boolean local) {
+        this.local = local;
+    }
+
 }
