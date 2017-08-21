c799ba5ec0f1d1f4136dbf5753b3b1bb80bbf3fb
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-21 23:58:00 +1200

    Added the content of the DataFetcher class as specified in the UML Class Diagram

---
+++ src/main/java/seng202/team5/DataFetcher.java
@@ -0,0 +1,28 @@
+package seng202.team5;
+//TODO fill in methods of the class
+
+public class DataFetcher {
+    private User user;
+    private String hostname;
+    private String credentials;
+
+    public void loadUser(User user) {
+
+    }
+
+    public void loadRoute(Route route) {
+
+    }
+
+    public void loadLocation(Location location) {
+
+    }
+
+    public void addUser(User user) {
+
+    }
+
+    public void addRoute(Route route) {
+
+    }
+}
--- src/main/java/seng202/team5/Map.java
+++ src/main/java/seng202/team5/Map.java
@@ -13,6 +13,6 @@ public class Map {
     }
 
     public void applyFilter(String filter) {
-        
+
     }
 }
