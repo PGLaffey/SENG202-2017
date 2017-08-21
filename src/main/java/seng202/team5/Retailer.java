07f2209413babdb8532181560959d8e22d6b3432
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-21 14:19:07 +1200

    Added the content of the Retailer class as specified in the UML Class Diagram

--- src/main/java/seng202/team5/Retailer.java
+++ src/main/java/seng202/team5/Retailer.java
@@ -1,4 +1,22 @@
 package seng202.team5;
 
-public class Retailer {
+
+public class Retailer extends Location {
+    //TODO Changed "type" to "product" as this avoids confusion with the "type" of location
+    private String product;
+    private String description;
+
+    public Retailer(double lattitude, double longitude, String name, String product, String description) {
+        super(lattitude, longitude, name, "Retailer");
+        this.product = product;
+        this.description = description;
+    }
+
+    public String getProduct() {
+        return this.product;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
 }
