bddc039f519c82ccbec609730ab33e59871ac717
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-21 23:52:40 +1200

    Added the content of the Map class as specified in the UML Class Diagram

---
+++ src/main/java/seng202/team5/Map.java
@@ -0,0 +1,18 @@
+package seng202.team5;
+
+public class Map {
+    private Route currentRoute;
+    private Location currentLocation;
+
+    public void loadRoute(Route route) {
+
+    }
+
+    public void loadLocation(Location location) {
+
+    }
+
+    public void applyFilter(String filter) {
+        
+    }
+}
