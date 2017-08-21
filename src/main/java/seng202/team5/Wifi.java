5a7e891b37845c08438af443b8353c1618b95b7e
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-21 14:20:26 +1200

    Added the content of the Wifi class as specified in the UML Class Diagram

--- src/main/java/seng202/team5/Wifi.java
+++ src/main/java/seng202/team5/Wifi.java
@@ -1,4 +1,14 @@
 package seng202.team5;
 
-public class Wifi {
+public class Wifi extends Location {
+    private String provider;
+
+    public Wifi(double lattitude, double longitude, String name, String provider) {
+        super(lattitude, longitude, name, "Wifi");
+        this.provider = provider;
+    }
+
+    public String getProvider() {
+        return this.provider;
+    }
 }
