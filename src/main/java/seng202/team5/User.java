5f585d6cbc2f1e007327abf32b158bab61f4c3f8
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-21 13:52:08 +1200

    Added the content of the User class as specified in the UML Class Diagram

--- src/main/java/seng202/team5/User.java
+++ src/main/java/seng202/team5/User.java
@@ -1,4 +1,53 @@
 package seng202.team5;
+import java.util.ArrayList;
 
 public class User {
+    private String name;
+    private int id;
+    private int routesCycled;
+    private double distanceCycled;
+    private double hoursCycled;
+    private ArrayList<Route> routes;
+    private ArrayList<Badge> badges;
+
+    //TODO Should this be on the UML?
+    public User(String name, int id) {
+        this.name = name;
+        this.id = id;
+        this.routesCycled = 0;
+        this.distanceCycled = 0;
+        this.hoursCycled = 0;
+        this.routes = new ArrayList<Route>();
+        this.badges = new ArrayList<Badge>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    //TODO Add getId(void) to the UML diagram
+    public int getId() {
+        return this.id;
+    }
+
+    public Route getRoute(int id) {
+        //TODO how are we using the route ID?
+        return this.routes[id];
+    }
+
+    public double getHours() {
+        return this.hoursCycled;
+    }
+
+    public double getDistance() {
+        return this.distanceCycled;
+    }
+
+    public int getRoutesCycled() {
+        return this.routesCycled;
+    }
+
+    public ArrayList<Badge> getBadges() {
+        return this.badges;
+    }
 }
