d326750b7f8fc79e83d611eeaf25fe67a2fc74cc
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-21 14:17:45 +1200

    Added the content of the Poi class as specified in the UML Class Diagram

--- src/main/java/seng202/team5/Poi.java
+++ src/main/java/seng202/team5/Poi.java
@@ -1,4 +1,20 @@
 package seng202.team5;
 
-public class Poi {
+public class Poi extends Location {
+    private String description;
+    private double cost;
+
+    public Poi(double lattitude, double longitude, String name, String description, double cost) {
+        super(lattitude, longitude, name, "Poi");
+        this.description = description;
+        this.cost = cost;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public double getCost() {
+        return this.cost;
+    }
 }
