8bc15dffe3525b5d32e3a117e54073dc94d00166
Jack Hay <jha233@uclive.ac.nz>
2017-08-22 17:59:34 +1200

    Checking that I can push from home

--- src/main/java/seng202/team5/Badge.java
+++ src/main/java/seng202/team5/Badge.java
@@ -1,5 +1,5 @@
 package seng202.team5;
-//TODO check that this is the correct import statement for image.
+//TODO check that this is the correct import statement for image. jack push test
 //TODO is requirement a String or an int; different in two places in the UML diagram.
 
 import java.awt.*;
