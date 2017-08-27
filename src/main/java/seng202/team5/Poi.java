aad77df0bb37cf1ef59d8835e2846124327cd8d8
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-27 21:57:34 +1200

    Edited the Poi class for style and added documentation.

--- src/main/java/seng202/team5/Poi.java
+++ src/main/java/seng202/team5/Poi.java
@@ -1,9 +1,20 @@
 package seng202.team5;
 
+/**
+ * Point of Interest (POI) subclass of location, stores a description of the location and the cost of visiting.
+ */
 public class Poi extends Location {
     private String description;
     private double cost;
 
+    /**
+     * The POI constructor, creates a new instance of the point of interest object using the location superclass constructor.
+     * @param lattitude The lattitude of the point of interest.
+     * @param longitude The longitude of the point of interest.
+     * @param name The name of the point of interest.
+     * @param description A description of the point of interest.
+     * @param cost The cost of visiting the point of interest.
+     */
     public Poi(double lattitude, double longitude, String name, String description, double cost) {
         super(lattitude, longitude, name, "Poi");
         this.description = description;
@@ -11,10 +22,10 @@ public class Poi extends Location {
     }
 
     public String getDescription() {
-        return this.description;
+        return description;
     }
 
     public double getCost() {
-        return this.cost;
+        return cost;
     }
 }
