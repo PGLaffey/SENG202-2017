8be23487aa6a2da17429c3297b6ac33bf93dfaa5
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-27 22:09:15 +1200

    Edited the Route class for style and added documentation.

--- src/main/java/seng202/team5/Route.java
+++ src/main/java/seng202/team5/Route.java
@@ -1,5 +1,8 @@
 package seng202.team5;
 
+/**
+ * The Route class, stores information about the route.
+ */
 public class Route {
     private String name;
     private int id; // If we make this a static variable, can we then ensure that the route id is unique?
@@ -10,9 +13,9 @@ public class Route {
 
     /**
      * Constructor for a Route.
-     * @param start
-     * @param end
-     * @param name
+     * @param start The start location of the route.
+     * @param end The end location of the route.
+     * @param name The name of the route.
      */
     public Route(Location start, Location end, String name) {
         this.start = start;
@@ -21,30 +24,30 @@ public class Route {
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
     //TODO Add getId(void) to the UML diagram
     public int getId() {
-        return this.id;
+        return id;
     }
 
     public double getDistance() {
-        return this.distance;
+        return distance;
     }
 
     //TODO Add getLocal to UML diagram
     public boolean getLocal() {
-        return this.local;
+        return local;
     }
 
     public Location getStart() {
-        return this.start;
+        return start;
     }
 
     //TODO Add getEnd(void) to the UML diagram, getStart added twice
     public Location getEnd() {
-        return this.end;
+        return end;
     }
 
     //TODO Should this be setLocation?
