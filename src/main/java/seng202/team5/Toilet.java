3b1a8cf09885edff92a5552caee1bcefd47facf9
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-27 22:05:42 +1200

    Edited the Toilet class for style and added documentation.

--- src/main/java/seng202/team5/Toilet.java
+++ src/main/java/seng202/team5/Toilet.java
@@ -1,10 +1,20 @@
 package seng202.team5;
 
-
+/**
+ * Toilet subclass of Location, stores whether the toilet has facilities for wheelchair users, and whether the toilet is unisex or not.
+ */
 public class Toilet extends Location {
     private boolean forDisabled;
     private boolean uniSex;
 
+    /**
+     * Toilet constructor, creates a new instance of the toilet object using the location superclass constructor.
+     * @param lattitude The lattitude of the toilet location.
+     * @param longitude The longitude of the toilet location.
+     * @param name The name of the toilet location.
+     * @param forDisabled Whether the toilet has facilities for wheelchair users.
+     * @param uniSex Whether the toilet is unisex or has seperate facilities for men and women.
+     */
     public Toilet(double lattitude, double longitude, String name, boolean forDisabled, boolean uniSex) {
         super(lattitude, longitude, name, "Toilet");
         this.forDisabled = forDisabled;
@@ -12,11 +22,11 @@ public class Toilet extends Location {
     }
 
     public boolean getForDisabled() {
-        return this.forDisabled;
+        return forDisabled;
     }
 
     public boolean getUniSex() {
-        return this.uniSex;
+        return uniSex;
     }
 }
 
