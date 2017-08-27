901f241409873e4fc00359b893249432c146436c
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-27 21:46:12 +1200

    Edited the Wifi class for style and added documentation.

--- src/main/java/seng202/team5/Wifi.java
+++ src/main/java/seng202/team5/Wifi.java
@@ -1,14 +1,24 @@
 package seng202.team5;
 
+/**
+ * Wifi subclass of Location, contains the information of the wifi provider.
+ */
 public class Wifi extends Location {
     private String provider;
 
+    /**
+     * Creates a new instance of the wifi class, uses the constructor of the Location superclass.
+     * @param lattitude The lattitude of the wifi hotspot.
+     * @param longitude The logitude of the wifi hotspot.
+     * @param name The name of the wifi hotspot location.
+     * @param provider The name of the provider of the wifi hotspot.
+     */
     public Wifi(double lattitude, double longitude, String name, String provider) {
         super(lattitude, longitude, name, "Wifi");
         this.provider = provider;
     }
 
     public String getProvider() {
-        return this.provider;
+        return provider;
     }
 }
