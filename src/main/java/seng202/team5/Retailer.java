8ccca0fc0a9ab692bb14cef7b3619aa24cb4f00e
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-08-27 21:49:54 +1200

    Edited the Retailer class for style and added documentation.

--- src/main/java/seng202/team5/Retailer.java
+++ src/main/java/seng202/team5/Retailer.java
@@ -1,11 +1,21 @@
 package seng202.team5;
 
-
+/**
+ * Retailer subclass of Location, contains the type of product sold, and a description of the retailer.
+ */
 public class Retailer extends Location {
     //TODO Changed "type" to "product" as this avoids confusion with the "type" of location
     private String product;
     private String description;
 
+    /**
+     * Constructor for the retailer subclass, uses the location superclass constructor.
+     * @param lattitude The lattitude of the retailer location.
+     * @param longitude The logitude of the retailer location.
+     * @param name The name of the retailer.
+     * @param product The product the retailer sells.
+     * @param description A description of the retailer at the location.
+     */
     public Retailer(double lattitude, double longitude, String name, String product, String description) {
         super(lattitude, longitude, name, "Retailer");
         this.product = product;
@@ -13,10 +23,10 @@ public class Retailer extends Location {
     }
 
     public String getProduct() {
-        return this.product;
+        return product;
     }
 
     public String getDescription() {
-        return this.description;
+        return description;
     }
 }
