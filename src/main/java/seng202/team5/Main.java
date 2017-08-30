87199675bc3d5b652fafe0a3b19f07e7b6b60fc0
courtney <courtney@SurfacePro3.request-it.local>
2017-08-31 08:52:41 +1200

    Main class for running the GUI

---
+++ src/main/java/seng202/team5/Main.java
@@ -0,0 +1,27 @@
+//Main class to set up and test the GUI until we incorporate it with the actual implementation
+
+package seng202.team5;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {    
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
+		primaryStage.setTitle("Login");
+		primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+	}
+	
+	public static void main(String[] args) {      
+		launch(args);
+	}
+	
+}
