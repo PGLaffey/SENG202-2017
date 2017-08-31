fe4be37edd1493e624e5dfd50856751c3eb96aaa
courtney <courtney@SurfacePro3.request-it.local>
2017-09-01 11:12:31 +1200

    Setting up IDs and Controller for the main screen

---
+++ src/main/java/seng202/team5/MainScreenController.java
@@ -0,0 +1,158 @@
+package seng202.team5;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class MainScreenController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button accountButton;
+
+    @FXML
+    private Button addLocationButton;
+
+    @FXML
+    private Button createRouteButton;
+
+    @FXML
+    private Button loadRouteButton;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Button mapButton;
+
+    @FXML
+    private Button randomRouteButton;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField searchInput;
+
+    @FXML
+    private Button shareIconButton;
+
+    @FXML
+    private Button shareLabelButton;
+
+    @FXML
+    private Button showFavButton;
+
+    @FXML
+    private Button showRetailerButton;
+
+    @FXML
+    private Button showRouteButton;
+
+    @FXML
+    private Button showToiletButton;
+
+    @FXML
+    private Button showWifiButton;
+
+    @FXML
+    private Button statButton;
+
+
+    @FXML
+    void accountPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void addLocationPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void createRoutePressed(ActionEvent event) {
+    }
+
+    @FXML
+    void loadRoutePressed(ActionEvent event) {
+    }
+
+    @FXML
+    void logoutPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void mapPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void randomRoutePressed(ActionEvent event) {
+    }
+
+    @FXML
+    void savePressed(ActionEvent event) {
+    }
+
+    @FXML
+    void shareIconPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void shareLabelPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void showFavPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void showRetailerPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void showRoutePressed(ActionEvent event) {
+    }
+
+    @FXML
+    void showToiletPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void showWifiPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void statPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void initialize() {
+        assert accountButton != null : "fx:id=\"accountButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert addLocationButton != null : "fx:id=\"addLocationButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert createRouteButton != null : "fx:id=\"createRouteButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert loadRouteButton != null : "fx:id=\"loadRouteButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert mapButton != null : "fx:id=\"mapButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert randomRouteButton != null : "fx:id=\"randomRouteButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert saveButton != null : "fx:id=\"saveButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert searchInput != null : "fx:id=\"searchInput\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert shareIconButton != null : "fx:id=\"shareIconButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert shareLabelButton != null : "fx:id=\"shareLabelButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert showFavButton != null : "fx:id=\"showFavButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert showRetailerButton != null : "fx:id=\"showRetailerButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert showRouteButton != null : "fx:id=\"showRouteButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert showToiletButton != null : "fx:id=\"showToiletButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert showWifiButton != null : "fx:id=\"showWifiButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+        assert statButton != null : "fx:id=\"statButton\" was not injected: check your FXML file 'MainScreen.fxml'.";
+
+
+    }
+
+}
