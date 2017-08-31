b896fd688d8e26f53c6a0cd2742d9eb92cb9b1e0
Jack Hay <jha233@uclive.ac.nz>
2017-08-31 12:21:32 +1200

    Changed 1 line

--- src/main/java/seng202/team5/Main.java
+++ src/main/java/seng202/team5/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception{
-		Parent root = FXMLLoader.load(getClass().getResource("/MainScreen.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("/LoginScreen.fxml"));
 		primaryStage.setTitle("Login");
 		primaryStage.setScene(new Scene(root, 300, 275));
 		primaryStage.show();
