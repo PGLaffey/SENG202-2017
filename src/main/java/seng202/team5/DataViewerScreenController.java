1c8edd3bc85be41be9373541925e19a2f1d40067
courtney <courtney@SurfacePro3.request-it.local>
2017-09-01 11:55:03 +1200

    Adding variables and methods to the login and data viewer screen controllers.

--- src/main/java/seng202/team5/DataViewerScreenController.java
+++ src/main/java/seng202/team5/DataViewerScreenController.java
@@ -1,70 +1,76 @@
 package seng202.team5;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 
-public class DataViewerScreenController {
-	
-	
-	@FXML
-	private Button mapButton;
-	
-	@FXML 
-	private Label mapPressedLabel;
-	
-	@FXML
-	private Button statButton;
-	
-	@FXML
-	private Button accountButton;
-	
-	@FXML
-	private Button logoutButton;	
-	
-	@FXML
-	private ChoiceBox filterGraphBox;
-	
-	@FXML
-	private RadioButton graphRadio;
-	
-	@FXML
-	private RadioButton rawDataRadio;
 
-	
-	/**
-	 * Method for when the Map
-	 * button is pressed
-	 */
-	public void mapPressed() {
-		mapPressedLabel.setVisible(true); //testing that the function works, but cant run it
-	}
-	
-	
-	/**
-	 * Method for when the Statistics
-	 * button is pressed
-	 */
-	public void statPressed() {
-	}
-	
-	
-	/**
-	 * Method for when the Account
-	 * button is pressed
-	 */
-	public void accountPressed() {
-	}
-	
-	
-	/**
-	 * Method for when the Logout
-	 * button is pressed
-	 */
-	public void logoutPressed() {
-	}
-	
-	
+public class DataViewerScreenController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button accountButton;
+
+    @FXML
+    private ChoiceBox<?> filterGraphBox;
+
+    @FXML
+    private RadioButton graphRadio;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private Button mapButton;
+
+    @FXML
+    private Label mapPressedLabel;
+
+    @FXML
+    private RadioButton rawDataRadio;
+
+    @FXML
+    private Button statButton;
+
+
+    @FXML
+    void accountPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void logoutPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void mapPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void statPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void initialize() {
+        assert accountButton != null : "fx:id=\"accountButton\" was not injected: check your FXML file 'DataViewerScreen.fxml'.";
+        assert filterGraphBox != null : "fx:id=\"filterGraphBox\" was not injected: check your FXML file 'DataViewerScreen.fxml'.";
+        assert graphRadio != null : "fx:id=\"graphRadio\" was not injected: check your FXML file 'DataViewerScreen.fxml'.";
+        assert logoutButton != null : "fx:id=\"logoutButton\" was not injected: check your FXML file 'DataViewerScreen.fxml'.";
+        assert mapButton != null : "fx:id=\"mapButton\" was not injected: check your FXML file 'DataViewerScreen.fxml'.";
+        assert mapPressedLabel != null : "fx:id=\"mapPressedLabel\" was not injected: check your FXML file 'DataViewerScreen.fxml'.";
+        assert rawDataRadio != null : "fx:id=\"rawDataRadio\" was not injected: check your FXML file 'DataViewerScreen.fxml'.";
+        assert statButton != null : "fx:id=\"statButton\" was not injected: check your FXML file 'DataViewerScreen.fxml'.";
+
+
+    }
+
 }
--- src/main/java/seng202/team5/LoginScreenController.java
+++ src/main/java/seng202/team5/LoginScreenController.java
@@ -1,43 +1,56 @@
 package seng202.team5;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+
 public class LoginScreenController {
-	
-	@FXML
-	private TextField usernameText;
-	
-	@FXML
-	private PasswordField passwordText;
-	
-	@FXML
-	private Button signInButton;
-	
-	@FXML
-	private Button signUpButton;
-	
-	@FXML
-	private CheckBox staySignedInCheck;
-	
 
-	/** 
-	 * Method for when the Sign In 
-	 * button is pressed
-	 */
-	public void signInPressed() {
-	}
-	
+    @FXML
+    private ResourceBundle resources;
 
-	/** 
-	 * Method for when the Sign Up 
-	 * button is pressed
-	 */
-	public void signUpPressed() {
-	}
-	
-	
+    @FXML
+    private URL location;
+
+    @FXML
+    private PasswordField passwordText;
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private CheckBox staySignedInCheck;
+
+    @FXML
+    private TextField usernameText;
+
+
+    @FXML
+    void signInPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void signUpPressed(ActionEvent event) {
+    }
+
+    @FXML
+    void initialize() {
+        assert passwordText != null : "fx:id=\"passwordText\" was not injected: check your FXML file 'LoginScreen.fxml'.";
+        assert signInButton != null : "fx:id=\"signInButton\" was not injected: check your FXML file 'LoginScreen.fxml'.";
+        assert signUpButton != null : "fx:id=\"signUpButton\" was not injected: check your FXML file 'LoginScreen.fxml'.";
+        assert staySignedInCheck != null : "fx:id=\"staySignedInCheck\" was not injected: check your FXML file 'LoginScreen.fxml'.";
+        assert usernameText != null : "fx:id=\"usernameText\" was not injected: check your FXML file 'LoginScreen.fxml'.";
+
+
+    }
+
 }
