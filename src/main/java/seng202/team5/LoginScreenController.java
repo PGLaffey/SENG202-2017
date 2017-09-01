bd271ce82695e9f7f8ddb47db84b6849f222772b
courtney <courtney@SurfacePro3.request-it.local>
2017-09-01 12:03:25 +1200

    Added the signin button method

--- src/main/java/seng202/team5/LoginScreenController.java
+++ src/main/java/seng202/team5/LoginScreenController.java
@@ -1,13 +1,18 @@
 package seng202.team5;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 
 public class LoginScreenController {
@@ -34,14 +39,30 @@ public class LoginScreenController {
     private TextField usernameText;
 
 
+    /**
+     * Method for when the SignIn
+     * Button is pressed.
+     * @param event
+     * @throws IOException
+     */
     @FXML
-    void signInPressed(ActionEvent event) {
+    void signInPressed(ActionEvent event) throws IOException { // At the moment it doesn't check that the username and password fields are filled in correctly
+    	
+    	Stage stage = (Stage) signInButton.getScene().getWindow();
+    	Parent root = FXMLLoader.load(getClass().getResource("/ProfileScreen.fxml"));
+    	
+    	Scene scene = new Scene(root);
+    	stage.setTitle("Profile");
+    	stage.setScene(scene);
+    	stage.show();    	
     }
 
+    
     @FXML
     void signUpPressed(ActionEvent event) {
     }
 
+    
     @FXML
     void initialize() {
         assert passwordText != null : "fx:id=\"passwordText\" was not injected: check your FXML file 'LoginScreen.fxml'.";
