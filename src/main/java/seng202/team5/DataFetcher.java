711c8199ea4e2188a6af3bcea9e6e860ba496c4a
PGLaffey <PGLaffey@DESKTOP-Q7588HV>
2017-09-04 01:06:58 +1200

    Adding more methods to DataFetcher

--- src/main/java/seng202/team5/DataFetcher.java
+++ src/main/java/seng202/team5/DataFetcher.java
@@ -98,7 +98,14 @@ public class DataFetcher {
      * @param user User to add into the external database
      */
     public void addUser(User user) {
-
+    	String firstName = user.getName();
+    	String username = user.getUsername();
+    	int routesCycled = user.getRoutesCycled();
+    	double hoursCycled = user.getHours();
+    	double distanceCycled = user.getDistance();
+    	runQuery("INSERT INTO tblUser "
+    			+ "(FName, Username, NumRoutesCycled, HoursCycled, DistanceCycled) VALUES "
+    			+ "('" + firstName + "', '" + username + "', '" + routesCycled + "', '" + hoursCycled + "', '" + distanceCycled + "')");
     }
 
     /**
@@ -106,9 +113,63 @@ public class DataFetcher {
      * @param route Route to add into the external database.
      */
     public void addRoute(Route route) {
-
+    	String name = route.getName();
+    	Location start = route.getStart();
+    	Location end = route.getEnd();
+    	
+    	int startID = findLocation(start);
+    	int endID = findLocation(end);
+    	
+    	if (startID == 0) {
+    		addLocation(start);
+    		startID = findLocation(start);
+    	}
+    	if (endID == 0) {
+    		addLocation(end);
+    		endID = findLocation(end);
+    	}
+    	
+    	runQuery("INSERT INTO tblRoutes "
+    			+ "(StartID, EndID, Name) VALUES "
+    			+ "('" + startID + "', '" + endID + "', '" + name + "')");
     }
 
+    /**
+     * 
+     * @param location The location to find in the database.
+     * @return The ID of the location in the database, returns 0 if location not found.
+     */
+    private int findLocation(Location location) {
+    	String locationID = "0";
+    	double[] coords = location.getCoords();
+    	int type = location.getLocationType();
+    	try {
+    		locationID = runQuery("SELECT LocationID FROM tblLocations "
+    				+ "WHERE Latitude = '" + coords[0] + "' AND "
+    				+ "Longitude = '" + coords[1] + "' AND "
+    				+ "Type = '" + type + "'").get(0).get(0);
+    	}
+    	catch (IndexOutOfBoundsException e) {
+    		locationID = "0";
+    	}
+    	return Integer.parseInt(locationID);
+    }
+    
+    /**
+     * Adds a location to the database
+     * @param location The Location to be added to the database.
+     */
+    public void addLocation(Location location) {
+    	String name = location.getName();
+    	int type = location.getLocationType();
+    	double[] coords = location.getCoords();
+    	double longitude = coords[1];
+    	double latitude = coords[0];
+    	runQuery("INSERT INTO tblLocations "
+    			+ "(Latitude, Longitude, Name, Type) VALUES "
+    			+ "('" + latitude + "', '" + longitude + "', '" + name + "', '" + type + "')");
+    }
+    
     /**
      * Provides a request to connect to the external database.
      * @throws ClassNotFoundException 
@@ -156,12 +217,14 @@ public class DataFetcher {
     //Purely for testing, will be removed later
     public static void main(String[] argv) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
     	DataFetcher test = new DataFetcher();
-    	//test.connectDbTest();
+    	//test.connectDbTest();				//Tests the connection to the database
     	test.connectDb();
     	Location testLoc1 = new Location(47.2134400, 172.1232100, "Telecom Hotspot", 3);
-    	//test.loadLocation(testLoc1);
+    	//test.loadLocation(testLoc1);		//Tests loading a valid location
     	Location testLoc2 = new Location(159.1547895, 0.3256984, "NULL", 4);
     	Route testRoute = new Route(testLoc1, testLoc2, "Test");
-    	test.loadRoute(testRoute);
+    	//test.loadRoute(testRoute);		//Tests loading a valid route
+    	Location invalidLoc = new Location(25.1258469, 56.1658468, "NULL", 4);
+    	//test.loadLocation(invalidLoc);	//Tests loading a invalid location
     }
 }
