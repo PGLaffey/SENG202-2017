083e1f2d096ad6dc85dba5c36aa06f74e4390ec7
courtney <courtney@SurfacePro3.request-it.local>
2017-09-05 14:21:18 +1200

    Created the fxml and controller class for changing password and deleting account. Can click through them.

---
+++ src/main/java/seng202/team5/DeleteAccountScreenController.java
@@ -0,0 +1,49 @@
+package seng202.team5;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+public class DeleteAccountScreenController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button noButton;
+
+    @FXML
+    private Button yesButton;
+
+
+    @FXML
+    void noButtonPressed(ActionEvent event) {
+    	Stage stage = (Stage) noButton.getScene().getWindow(); 
+    	stage.hide();	
+    }
+
+    @FXML
+    void yesButtonPressed(ActionEvent event) {
+    	Stage stage = (Stage) yesButton.getScene().getWindow(); 
+    	stage.hide();	// TODO: Work out how to then change primaryStage to login screen
+    }
+
+    @FXML
+    void initialize() {
+        assert noButton != null : "fx:id=\"noButton\" was not injected: check your FXML file 'DeleteAccountScreen.fxml'.";
+        assert yesButton != null : "fx:id=\"yesButton\" was not injected: check your FXML file 'DeleteAccountScreen.fxml'.";
+
+
+    }
+
+}
