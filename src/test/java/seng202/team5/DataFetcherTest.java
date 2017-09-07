ef2d917a36508908258e116229e1f215479352c7
Eiran Ling <eli26@uclive.ac.nz>
2017-09-07 22:59:44 +1200

    Added test methods

--- src/test/java/seng202/team5/DataFetcherTest.java
+++ src/test/java/seng202/team5/DataFetcherTest.java
@@ -1,11 +1,16 @@
 package seng202.team5;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.junit.Ignore;
+import org.junit.Test;
+
+import java.sql.SQLException;
 
 public class DataFetcherTest extends TestCase {
+    private DataFetcher fetcher;
+    private static final String TARGET = "/testdata/";
+
     /**
      * @param testName The name of the test
      */
@@ -17,18 +22,90 @@ public class DataFetcherTest extends TestCase {
     /**
      * @return The suite which the test belongs to.
      */
-    public static Test suite()
+    public static junit.framework.Test suite()
     {
         return new TestSuite(DataFetcherTest.class);
     }
 
     /**
-     * Skeleton test
+     * Sets up a new dataFetcher before every test
      */
-    //TODO: Add real tests
-    @Ignore
-    public void testBlueSky()
+    public void setUp()
     {
-        assertTrue(true);
+        fetcher = new DataFetcher();
     }
+    /**
+     * Test to ensure that the connectDB function results in a functional connection.
+     */
+    @Test
+    public void testConnectDB()
+    {
+        try {
+            fetcher.connectDb();
+        } catch (IllegalAccessException exception) {
+            fail("Call to access illegal area.");
+        } catch (ClassNotFoundException exception) {
+            fail("Class not found");
+        } catch (InstantiationException exception) {
+            fail("Failed to instantiate a connection");
+        }
+        try {
+            // Checks if the connection obtained by the fetcher in connectDB is valid
+            assertTrue(fetcher.getConnect().isValid(60));
+        } catch (SQLException exception) {
+            fail("SQLException");
+        }
+    }
+
+    /**
+     * Test to ensure the DataFetcher class can be sent SQL statements and return correct data.
+     */
+    @Test
+    public void testSQL()
+    {
+
+    }
+
+    /**
+     * Test to ensure that the DataFetcher class can write routes to the database.
+     */
+    @Test
+    public void testWriteRoute()
+    {
+        FileManager reader = new FileManager();
+        reader.readFile(TARGET+"route_data1.csv");
+
+    }
+
+    /**
+     * Test to ensure that the DataFetcher class can write Wifi hotspots to the database.
+     */
+    @Test
+    public void testWriteWifi()
+    {
+
+    }
+
+    /**
+     * Test to ensure that the DataFetcher class can write Retailers to the database correctly.
+     */
+    @Test
+    public void testWriteRetailer()
+    {
+
+    }
+
+    /**
+     * Test to ensure that the DataFetcher class closes its connection.
+     */
+    @Test
+    public void testClose()
+    {
+
+    }
+
+    /**
+     * Test to ensure that the DataFetcher class can add new users to the database correctly.
+     */
+
 }
