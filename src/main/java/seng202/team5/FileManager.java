b62adb5ca86e4f1e4f1741cf6728b1adf7c8d2ed
RebeckaCox <30788887+RebeckaCox@users.noreply.github.com>
2017-09-08 17:58:36 +1200

    Edited and added methods to the FileManager class regarding the local storage of the route objects and data.

--- src/main/java/seng202/team5/FileManager.java
+++ src/main/java/seng202/team5/FileManager.java
@@ -2,8 +2,12 @@ package seng202.team5;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
+/**
+ * File Manager class reads and writes files to and from .csv files, and stores the information in the apps current storage.
+ * The File Manager class also serializes and deserializes user objects.
+ * @author Rebecka Cox
+ */
 public class FileManager {
 
     /**
@@ -83,9 +87,7 @@ public class FileManager {
             PrintWriter printWriter = new PrintWriter(new File(fileName));
             StringBuilder stringBuilder = new StringBuilder();
             for (String line : content) {
-                for (String item : line.split(",")) {
-                    stringBuilder.append(item);
-                }
+                stringBuilder.append(line);
                 stringBuilder.append('\n');
             }
             printWriter.write(stringBuilder.toString());
@@ -97,19 +99,63 @@ public class FileManager {
 
 
     /**
+     * Retrieves a list of routes from the readFile function and then converts them indevidually to Route objects stored
+     * in the current storage class for that instance of the app.
+     * @param currentStorage The instance of the CurrentStorage class for the open instance of the app.
+     */
+    public void routeRetriever(CurrentStorage currentStorage) {
+        ArrayList<String> routes = readFile("SENG202-Team5-Cyclr/src/main/resources/data_files/2014-01 - Citi Bike trip data.csv");
+        routes.remove(0);
+        for (String route : routes) {
+            String[] information = route.split(",");
+            String startName = information[4];
+            String endName = information[8];
+            double startLatitude = Double.parseDouble(information[5]);
+            double startLongitude = Double.parseDouble(information[6]);
+            double endLatitude = Double.parseDouble(information[9]);
+            double endLongitude = Double.parseDouble(information[10]);
+            Location startLocation = new Location(startLatitude, startLongitude, startName);
+            Location endLocation = new Location(endLatitude, endLongitude, endName);
+            Route newRoute = new Route(startLocation, endLocation);
+            currentStorage.addRoute(newRoute);
+        }
+    }
+
+    /**
+     * Stores the route data stored in the current storage class.
+     * @param currentStorage The current instance of the CurrentStorage class of the application.
+     */
+    public void routeWriter(CurrentStorage currentStorage) {
+        ArrayList<Route> routes = currentStorage.getRouteArray();
+        ArrayList<String> strRoutes = new ArrayList<String>();
+        for (Route route : routes) {
+            String strRoute = "";
+            String startName = route.getStart().getName();
+            String endName = route.getEnd().getName();
+            String startLatitude = Double.toString(route.getStart().getCoords()[0]);
+            String startLongitude = Double.toString(route.getStart().getCoords()[1]);
+            String endLatitude = Double.toString(route.getEnd().getCoords()[0]);
+            String endLongitude = Double.toString(route.getEnd().getCoords()[1]);
+            strRoute = startName + "," + startLatitude + "," + startLongitude + "," + endName + "," + endLatitude + "," + endLongitude;
+            strRoutes.add(strRoute);
+        }
+        writeFile("SENG202-Team5-Cyclr/src/main/resources/data_files/route_data.csv", strRoutes);
+    }
+
+
+    /**
      * Retrieves the list of the given retailers and converts each of these to being a new instance of Retailer stored in currentStorage.
      * @param currentStorage The instance of the current storage object that the list of retailers will be added to.
      */
     public void retailerRetriever(CurrentStorage currentStorage) {
         ArrayList<String> retailers = readFile("SENG202-Team5-Cyclr/src/main/resources/data_files/Lower_Manhattan_Retailers.csv");
-        int i = 2;
-        while (i < retailers.size()) {
-            for (String retailer : retailers) {
-                String[] information = retailer.split(",");
-                String retailerAddress = information[1] + information[2] + information[3] + information[4] + information[5] + information[6];
-                Retailer newRetailer = new Retailer(retailerAddress, information[0], information[7], information[8]);
-                currentStorage.addRetailer(newRetailer);
-            }
+        retailers.remove(0);
+        retailers.remove(0);
+        for (String retailer : retailers) {
+            String[] information = retailer.split(",");
+            String retailerAddress = information[1] + information[2] + information[3] + information[4] + information[5] + information[6];
+            Retailer newRetailer = new Retailer(retailerAddress, information[0], information[7], information[8]);
+            currentStorage.addRetailer(newRetailer);
         }
     }
 
@@ -120,16 +166,14 @@ public class FileManager {
      */
     public void wifiRetriever(CurrentStorage currentStorage) {
         ArrayList<String> wifiHotspots = readFile("SENG202-Team5-Cyclr/src/main/resources/data_files/NYC_Free_Public_WiFi_03292017.csv");
-        int i = 1;
-        while (i < wifiHotspots.size()) {
-            for (String wifiHotspot: wifiHotspots) {
-                String[] information = wifiHotspot.split(",");
-                Double wifiLatitude = new Double(information[7]);
-                Double wifiLongitude = new Double(information[8]);
-                String wifiName = information[0];
-                String wifiProvider = information[4];
-                Wifi newHotspot = new Wifi(wifiLatitude, wifiLongitude, wifiName, wifiProvider);
-            }
+        wifiHotspots.remove(0);
+        for (String wifiHotspot: wifiHotspots) {
+            String[] information = wifiHotspot.split(",");
+            Double wifiLatitude = new Double(information[7]);
+            Double wifiLongitude = new Double(information[8]);
+            String wifiName = information[0];
+            String wifiProvider = information[4];
+            Wifi newHotspot = new Wifi(wifiLatitude, wifiLongitude, wifiName, wifiProvider);
         }
     }
 }
--- src/main/java/seng202/team5/Route.java
+++ src/main/java/seng202/team5/Route.java
@@ -14,7 +14,20 @@ public class Route {
     private Location via3;
 
     /**
-     * Constructor for a Route.
+     * Constructs a route based on the start and end locations.
+     * @param start The start location of the route.
+     * @param end The end location of the route
+     */
+    public Route(Location start, Location end) {
+        this.start = start;
+        this.end = end;
+        via1 = null;
+        via2 = null;
+        via3 = null;
+    }
+
+    /**
+     * Overloaded constructor for the route class, gives the route a name.
      * @param start The start location of the route.
      * @param end The end location of the route.
      * @param name The name of the route.
@@ -32,12 +45,10 @@ public class Route {
         return name;
     }
 
-
     public double getDistance() {
         return distance;
     }
 
-    //TODO Add getLocal to UML diagram
     public boolean getLocal() {
         return local;
     }
@@ -50,15 +61,12 @@ public class Route {
     	Location[] vias = {via1, via2, via3};
     	return vias;
     }
-    
-    //TODO Add getEnd(void) to the UML diagram, getStart added twice
+
     public Location getEnd() {
         return end;
     }
 
-    //TODO Should this be setLocation?
     public void storeLocation(boolean local) {
         this.local = local;
     }
-
 }
