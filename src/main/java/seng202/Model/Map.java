a9b0d315d3f6757ed915b9fb0ec27f3cbe3a8f13
Eiran Ling <eiranling@gmail.com>
2017-09-14 13:03:43 +1200

    updating map

--- src/main/java/seng202/Model/Map.java
+++ src/main/java/seng202/Model/Map.java
@@ -1,12 +1,23 @@
 package seng202.Model;
 
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.javascript.object.DirectionsPane;
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
 import com.lynden.gmapsfx.javascript.object.LatLong;
 import com.lynden.gmapsfx.service.directions.*;
 
-public class Map {
+public class Map implements DirectionsServiceCallback {
     private Route currentRoute;
     private Location currentLocation;
     private static DirectionsService directionsServiceHandler = new DirectionsService();
+    private DirectionsResult result;
+    private DirectionStatus status;
+
+    @Override
+    public void directionsReceived(DirectionsResult results, DirectionStatus status) {
+        result = results;
+        this.status = status;
+    }
 
     public static double getLattitude(String address) {
         DirectionsRequest request = new DirectionsRequest(address, address, TravelModes.BICYCLING);
@@ -26,13 +37,21 @@ public class Map {
         DirectionsRequest request = new DirectionsRequest(new LatLong(srcLat, srcLong),
                             new LatLong(destLat, destLong),
                 TravelModes.BICYCLING);
-        DirectionsService servicer = new DirectionsService();
+        Map.findRoute(request);
+        for (DirectionsRoute route : result.getRoutes()) {
+
+        }
         DirectionsLeg dirLeg = new DirectionsLeg();
 
         return dirLeg.getDistance().getValue();
     }
 
-    public void loadRoute(Route route) {
+    public void findRoute(DirectionsRequest request) {
+        GoogleMapView mapView = new GoogleMapView();
+        GoogleMap map = mapView.createMap();
+
+        DirectionsPane dirPane = mapView.getDirec();
+        directionsServiceHandler.getRoute(request, this, new DirectionsRenderer(true, map, dirPane));
 
     }
 
