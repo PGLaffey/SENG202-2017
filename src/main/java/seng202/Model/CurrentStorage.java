d5fd95020cebda8a62864c7c242b407475d9c24b
courtney <courtney@SurfacePro3.request-it.local>
2017-09-21 20:18:08 +1200

    Removed duplicate methods

--- src/main/java/seng202/Model/CurrentStorage.java
+++ src/main/java/seng202/Model/CurrentStorage.java
@@ -120,10 +120,6 @@ public class CurrentStorage {
 		generalArray.add(location);
 	}
 	
-	public static void addNewGeneral(Location location) {
-		generalArray.add(location);
-		addedGeneral.add(generalArray.indexOf(location));
-	}
 	
     public static void addRoute(Route route) {
         if (!(routeArray.contains(route))) {
@@ -217,11 +213,6 @@ public class CurrentStorage {
     		wifiArray.add(wifi);
 		}
 	}
-    
-    public static void addNewWifi(Wifi wifi) {
-    	wifiArray.add(wifi);
-    	addedWifi.add(wifiArray.indexOf(wifi));
-    }
 
 	public static boolean containsToilet(Toilet toilet) {
     	for (Toilet currentToilet : CurrentStorage.getToiletArray()) {
@@ -238,18 +229,8 @@ public class CurrentStorage {
 		}
     }
     
-    public static void addNewToilet(Toilet toilet) {
-    	toiletArray.add(toilet);
-    	addedToilets.add(toiletArray.indexOf(toilet));
-    }
-    
     public static void addPoi(Poi poi) {
     	poiArray.add(poi);
-    }
-    
-    public static void addNewPoi(Poi poi) {
-    	poiArray.add(poi);
-    	addedPoi.add(poiArray.indexOf(poi));
     }
 
     public static void addCoords(Coord coord) { 
