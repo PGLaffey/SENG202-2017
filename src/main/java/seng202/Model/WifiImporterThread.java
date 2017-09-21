f3ef4e4daeaf218bd95378d35fc24d176eadf1cf
Eiran Ling <eiranling@gmail.com>
2017-09-22 10:37:05 +1200

    Fixing tests to comply with RawDataViewer

---
+++ src/main/java/seng202/Model/WifiImporterThread.java
@@ -0,0 +1,17 @@
+package seng202.Model;
+
+/**
+ * Thread to import Wifi locations in a separate thread.
+ * Created by eli26 on 22/09/17.
+ */
+public class WifiImporterThread implements Runnable {
+    private String path;
+
+    public WifiImporterThread(String path) {
+        this.path = path;
+    }
+
+    public void run(){
+        FileManager.wifiRetriever(path);
+    }
+}
