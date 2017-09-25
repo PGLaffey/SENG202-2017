3bd2bf1a21e78a19aceabe2c53ac64d359c9aba0
rco61@uclive.ac.nz <rco61@uclive.ac.nz>
2017-09-26 02:09:56 +1300

    Changes made to the location of the main class

---
+++ src/main/java/seng202/Controller/Main.java
@@ -0,0 +1,43 @@
+//Main class to set up and test the GUI until we incorporate it with the actual implementation
+
+package seng202.Controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import seng202.Model.CurrentStorage;
+import seng202.Model.DataFetcher;
+import seng202.Model.FileManager;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(Main.class.getResource("/LoginScreen.fxml"));
+		primaryStage.setTitle("Login");
+		primaryStage.setScene(new Scene(root, 300, 275));
+		primaryStage.show();
+	}
+
+	@Override
+	public void stop() {
+		DataFetcher exporter = new DataFetcher();
+		try {
+			exporter.connectDb();
+			exporter.storeCurrentStorage();
+			exporter.closeConnection();
+			FileManager.userSerialize(CurrentStorage.getUser(), "./src/main/resources/data_files/");
+			CurrentStorage.flush();
+			System.exit(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
