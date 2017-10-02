2d63c769e4128973cf3d7bd4c22865fda85a5bc1
Eiran Ling <eiranling@gmail.com>
2017-10-03 10:49:26 +1300

    Fixed SQL errors on close

--- src/main/java/seng202/Controller/LoginScreenController.java
+++ src/main/java/seng202/Controller/LoginScreenController.java
@@ -6,10 +6,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
-import seng202.Model.*;
+import seng202.Model.CurrentStorage;
+import seng202.Model.DataFetcher;
+import seng202.Model.User;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public class LoginScreenController {
@@ -50,14 +50,6 @@ public class LoginScreenController {
 					incorrectUserLbl.setVisible(true);
 				} else if ((data.fetchPassword(usernameText.getText()).equals(passwordText.getText().toString()))) {
 
-					Thread retailerImporter = new Thread(new RetailerImporterThread(new File(getClass().getResource("/data_files/").getFile()).toString() + "/Retailers_subset.csv"));
-					retailerImporter.start();
-					Thread wifiImporter = new Thread(new WifiImporterThread(new File(getClass().getResource("/data_files/").getFile()).toString() + "/NYC_Free_Public_WiFi_03292017.csv"));
-					wifiImporter.start();
-			//new File(getClass().getResource("/data_files/").getFile()).toString() +"/2014-01 - Citi Bike trip data.csv"
-					//Thread routeImporter = new Thread(new RouteImporterThread(new File(getClass().getResource("/data_files/2014-01 - Citi Bike trip data.csv").getPath().replaceAll("%20", " ")).getPath()));
-					//routeImporter.start();
-					//Thread routeImporter(new File(getClass().getResource("/data_files/").getFile()).toString() + "/2014-02 - Citi Bike trip data.csv"));
 					ArrayList<String> userInfo;
 					userInfo = data.fetchUserInfo(usernameText.getText());
 
