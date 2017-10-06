5bdf17b000b06f339f4664af77351b8814ab411e
PGLaffey <PGLaffey@DESKTOP-Q7588HV>
2017-10-07 03:16:03 +1300

    Quick Fix 3.0

--- src/main/java/seng202/Model/DataFetcher.java
+++ src/main/java/seng202/Model/DataFetcher.java
@@ -743,12 +743,15 @@ public class DataFetcher {
     		//Initialize the query to fetch all Locations from the database and its result set
     		PreparedStatement qryLoadLocations = connect.prepareStatement("SELECT * FROM tblLocations");
 			ResultSet output = qryLoadLocations.executeQuery();
-			boolean secret = false;
+			boolean secret = true;
 			int ownerID = 0;
 	    	//Loop while there a another Location to be loaded from the database
 			while (output.next()) {
-				secret = output.getBoolean(6);
+				if (output.getBoolean(6)) {
+					secret = false;
+				}
 				//Checks if the Location is set to be secret
+				System.out.println("Location: " + output.getInt(1) + " | Secret: " + secret);
 				if (secret) {
 					ownerID = output.getInt(5);
 					//Checks if the Location belongs to a User
@@ -765,6 +768,7 @@ public class DataFetcher {
 						}
 					}
 				}
+				secret = true;
 			}
 		} 
     	//Prints the correct error statements if an SQLException occurs
@@ -1208,7 +1212,7 @@ public class DataFetcher {
     	try {
     		//Tells the driver manager of jdbc to create a connection to a database of type mysql with the ip 222.152.179.135, through port 3306, named cyclrr, using user 'monitor' and password 'Team5Pass'
     		//Following line the 192.168.1.70 needs to be 125.239.188.8 if outside of Patrick's network
-    		connect = DriverManager.getConnection("jdbc:mysql://125.239.188.8:3306/cyclrr","monitor","Team5Pass");
+    		connect = DriverManager.getConnection("jdbc:mysql://192.168.1.70:3306/cyclrr","monitor","Team5Pass");
     	}
     	catch (SQLException ex) {
     		printSqlError(ex);
@@ -1254,7 +1258,22 @@ public class DataFetcher {
 
     
     public static void main(String[] argv) {
-    	
+    	DataFetcher da = new DataFetcher();
+    	try {
+			da.connectDb();
+			da.loadAllLocations();
+			System.out.println(CurrentStorage.getToiletArray());
+			
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
