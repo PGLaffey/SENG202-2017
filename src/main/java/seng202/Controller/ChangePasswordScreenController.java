470998893441d3884e48d475ca60049f0c8537a8
PGLaffey <PGLaffey@DESKTOP-Q7588HV>
2017-10-09 01:00:36 +1300

    Final Style Check For Controllers: ChangePassword, ConfirmationDialog...

--- src/main/java/seng202/Controller/ChangePasswordScreenController.java
+++ src/main/java/seng202/Controller/ChangePasswordScreenController.java
@@ -18,6 +18,9 @@ import seng202.Model.CurrentStorage;
 import seng202.Model.DataFetcher;
 import seng202.Model.User;
 
+/**
+ * Controller for the change password screen.
+ */
 public class ChangePasswordScreenController {
 
     @FXML
@@ -53,8 +56,7 @@ public class ChangePasswordScreenController {
     
     /** 
      * Method for when the cancel button is pressed, closes the window.
-     * @param event
-     * @throws IOException
+     * @param event - Auto-generated event when button is pressed
      */
     @FXML
     void cancelButtonPressed(ActionEvent event) throws IOException {  	
@@ -65,11 +67,13 @@ public class ChangePasswordScreenController {
 
     /**
      * Method for when the change password button is pressed.
-     * Verifies the old and new password then runs the database query to update the users password
-     * @param event Auto-generated event when button is pressed
+     * Verifies the old and new password then runs the database query to update the users 
+     * password.
+     * @param event - Auto-generated event when button is pressed
      */
     @FXML
-    void changePasswordButtonPressed(ActionEvent event) throws IOException, IllegalAccessException, ClassNotFoundException, InstantiationException {
+    void changePasswordButtonPressed(ActionEvent event) throws IOException, 
+    		IllegalAccessException, ClassNotFoundException, InstantiationException {
         User user = CurrentStorage.getUser();
         DataFetcher data = new DataFetcher();
         data.connectDb();
@@ -90,12 +94,20 @@ public class ChangePasswordScreenController {
     }
 
     
+    /**
+     * Initializes the controller.
+     */
     @FXML
     void initialize() {
-        assert cancelButton != null : "fx:id=\"cancelButton\" was not injected: check your FXML file 'ChangePasswordScreen.fxml'.";
-        assert changePasswordButton != null : "fx:id=\"changePasswordButton\" was not injected: check your FXML file 'ChangePasswordScreen.fxml'.";
-        assert newPasswordText != null : "fx:id=\"newPasswordText\" was not injected: check your FXML file 'ChangePasswordScreen.fxml'.";
-        assert newPasswordTextAgain != null : "fx:id=\"newPasswordTextAgain\" was not injected: check your FXML file 'ChangePasswordScreen.fxml'.";
-        assert oldPasswordText != null : "fx:id=\"oldPasswordText\" was not injected: check your FXML file 'ChangePasswordScreen.fxml'.";
+        assert cancelButton != null : "fx:id=\"cancelButton\" was not injected: check your FXML "
+        		+ "file 'ChangePasswordScreen.fxml'.";
+        assert changePasswordButton != null : "fx:id=\"changePasswordButton\" was not injected: "
+        		+ "check your FXML file 'ChangePasswordScreen.fxml'.";
+        assert newPasswordText != null : "fx:id=\"newPasswordText\" was not injected: check your "
+        		+ "FXML file 'ChangePasswordScreen.fxml'.";
+        assert newPasswordTextAgain != null : "fx:id=\"newPasswordTextAgain\" was not injected: "
+        		+ "check your FXML file 'ChangePasswordScreen.fxml'.";
+        assert oldPasswordText != null : "fx:id=\"oldPasswordText\" was not injected: check your "
+        		+ "FXML file 'ChangePasswordScreen.fxml'.";
     }
 }
