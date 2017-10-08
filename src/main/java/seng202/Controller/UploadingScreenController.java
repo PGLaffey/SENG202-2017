e531cf93c80c35979a95508f2e86cdb3a536a8dc
PGLaffey <PGLaffey@DESKTOP-Q7588HV>
2017-10-09 03:27:26 +1300

    Final Style Check For Controllers: ToiletInfo, Uploading, WifiInfo

--- src/main/java/seng202/Controller/UploadingScreenController.java
+++ src/main/java/seng202/Controller/UploadingScreenController.java
@@ -10,7 +10,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
 /**
  * Controller for the uploading screen
  */
@@ -19,10 +18,10 @@ public class UploadingScreenController {
     @FXML
     Button cancelButton;
 
+    
     /**
-     * Method to execute when the cancel button is pressed
-     * @param event Auto generated event
-     * @throws IOException
+     * Method to execute when the cancel button is pressed.
+     * @param event - Auto generated event
      */
     @FXML
     void cancelPressed(ActionEvent event) throws IOException {
@@ -34,8 +33,13 @@ public class UploadingScreenController {
         stage.show();
     }
 
+    
+    /**
+     * Initializes the controller.
+     */
     @FXML
     void initialize() {
-        assert cancelButton != null : "fx:id=\"cancelButton\" was not injected: check your FXML file 'ConfirmationDialogScreen.fxml'.";
+        assert cancelButton != null : "fx:id=\"cancelButton\" was not injected: check your FXML"
+        		+ " file 'ConfirmationDialogScreen.fxml'.";
     }
 }
